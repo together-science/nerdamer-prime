4625b055e2d1353e0a512ec675c1a59f60080071
Gunnar Mein <gunnar@together.science>
2023-04-28 15:41:20 -0700

    tests for all active issues

--- spec/unsolved.spec.js
+++ spec/unsolved.spec.js
@@ -8,9 +8,19 @@ require('../Calculus.js');
 require('../Solve.js');
 
 describe('Known problems:', function () {
-    it('Algebra', function () {       
-        expect(nerdamer('baseunit_m^(-1)*sqrt(baseunit_m^2*cos(3)+baseunit_m^2)').evaluate().text())
+    it('Algebra', function () {    
+        // issue #1 - baseunit_m should cancel out 
+        expect(nerdamer('baseunit_m^(-1)*sqrt(baseunit_m^2*cos(3)+baseunit_m^2)')
+            .evaluate().text())
             .toEqual('0.100037509962788179');
+        // issue #7 - everything becomes 1
+        expect(nerdamer("log(0.01s)/(-239263565+51955423log(s))-1")
+            .simplify().text())
+            .not.toEqual('1');
+        // issue #11 - lost precision
+        expect(nerdamer("(19279880988/100000000000000000)kg(1/((km)^(3)))(((((1km))/((1000m))))^(3))")
+            .text("decimals"))
+            .toEqual("1.9279880988e-16*kg*m^(-3)");
     });
     it('Calculus', function () {
         expect(nerdamer('limit(cos(sin(x)+2), x, Infinity)').toString()).toEqual('[cos(1),cos(3)]');
