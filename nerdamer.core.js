77a2e26defb11d8e3f8735faa56f93c82be96212
gunnarmein-ts <gunnar@together.science>
2025-08-14 09:30:02 -0700

    test 200

--- nerdamer.core.js
+++ nerdamer.core.js
@@ -134,7 +134,7 @@ var nerdamer = (function (imports) {
         //True if ints should not be converted to
         SCIENTIFIC_IGNORE_ZERO_EXPONENTS: true,
         // no simplify() or solveFor() should take more ms than this
-        TIMEOUT: 800,
+        TIMEOUT: 200,
     };
 
     (function () {
